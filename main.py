8c8d5da20efd3e8ff00c6771c342a7a663748bc1
Jorge-Gagliardo <jorge.gagliardo1995@gmail.com>
2024-03-03 19:49:57 -0500

    operaciones basicas en python

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 def sumar(a, b):
-    # aqui se realiza la suma de dos numeros
+    return a + b
+
 
 def restar(a, b):
-    # aqui se realiza la resta de dos numeros
+    return a - b
+
 
 def multiplicar(a, b):
-    # aqui se realiza la multiplicacion de dos numeros
+    return a * b
+
 
 def dividir(a, b):
-    # aqui se realiza la division de dos numeros
+    if b == 0:
+        return 'Error: No se puede dividir por cero'
+    else:
+        return a / b
+
 
 def main():
     num1 = float(input("Ingrese el primer número: "))
@@ -18,6 +25,7 @@ def main():
     print("Resta:", restar(num1, num2))
     print("Multiplicación:", multiplicar(num1, num2))
     print("División:", dividir(num1, num2))
+
 
 if __name__ == "__main__":
     main()
